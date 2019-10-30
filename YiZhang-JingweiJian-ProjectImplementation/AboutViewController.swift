20b54add7be0c2e2df7e03814beb8fdc8e872a87
YiZhang <cattyzhang0110@gmail.com>
2019-10-30 22:50:04 +1100

    UI v1.0

---
+++ YiZhang-JingweiJian-ProjectImplementation/AboutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.swift
+//  YiZhang-JingweiJian-ProjectImplementation
+//
+//  Created by Yi Zhang on 30/10/19.
+//  Copyright © 2019 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let backgroundImage = UIImage.init(named: "home_page")
+        let backgroundImageView = UIImageView.init(frame: self.view.frame)
+
+        backgroundImageView.image = backgroundImage
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.alpha = 0.3
+
+        self.view.insertSubview(backgroundImageView, at: 0)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
